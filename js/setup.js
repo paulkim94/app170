9ff9a8120fe633364a7d0ca47bf529839c2b69dd
Kathy Huynh <kah034@ucsd.edu>
2016-02-11 22:34:48 -0800

    setup guide

--- js/setup.js
+++ js/setup.js
@@ -12,19 +12,42 @@ $(document).ready(function() {
  	$("#nextButton1").click(function(e) {
  		console.log("Next Clicked");
  		$(".setup1").addClass("hidden");
- 		$("p:first").addClass("intro");
+ 		$(".setup2").removeClass("hidden");
  	});
 
- 	var step3Content = "<div class='setup3'><h3>GOAL: I want to save up $500 for a trip to New York by July 2016.</h3><hr><h3>I usually spend $50 per week.</h3><hr><h2>Step 3: Choose a weekly or monthly budget.</h2><p>To reach your goal, you need to save $100 each month, or $25 each week.</p><form><input type='radio' name='budgetOption' value='weekly'>From now on, I will plan to spend only $100 per month.<br><input type='radio' name='budgetOption' value='weekly'>From now on, I will plan to spend only $100 per month.> Female<br></form><div class='buttons'><a class='btn-piggy' id='backButton2' href='#'>Back &lsaquo;</a><a class='btn-piggy' id='nextButton2' href='#'>Next &rsaquo;</a></div></div>";
  	$("#nextButton2").click(function(e) {
  		console.log("Next Clicked");
- 		$(".setup").html(step3Content);
+ 		$(".setup2").addClass("hidden");
+ 		$(".setup3").removeClass("hidden");
  	});
 
  	$("#backButton2").click(function(e) {
  		console.log("Back Clicked");
- 		$(".setup").html(".setup1");
+ 		$(".setup2").addClass("hidden");
+ 		$(".setup1").removeClass("hidden");
  	});
 
- 	
+ 	$("#nextButton3").click(function(e) {
+ 		console.log("Next Clicked");
+ 		$(".setup3").addClass("hidden");
+ 		$(".setup4").removeClass("hidden");
+ 	});
+
+ 	$("#backButton3").click(function(e) {
+ 		console.log("Back Clicked");
+ 		$(".setup3").addClass("hidden");
+ 		$(".setup2").removeClass("hidden");
+ 	});
+
+ 	$("#nextButton4").click(function(e) {
+ 		console.log("Next Clicked");
+ 		$(".setup4").addClass("hidden");
+ 		$(".setup5").removeClass("hidden");
+ 	});
+
+ 	$("#backButton4").click(function(e) {
+ 		console.log("Back Clicked");
+ 		$(".setup4").addClass("hidden");
+ 		$(".setup3").removeClass("hidden");
+ 	});
  }
