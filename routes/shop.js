366b6204d4f354ac32e5ef73a1f89174bf621118
Kathy Huynh <kah034@ucsd.edu>
2016-02-17 19:20:25 -0800

    shop purchase

--- routes/shop.js
+++ routes/shop.js
@@ -1,26 +1,8 @@
 /*
  * GET shop page.
  */
+var data = require("../shop.json");
 
 exports.view = function(req, res){
-  res.render('shop', {"storeItems": [
-		{
-			"ItemName": "Blue Shades",
-			"ItemThumbnail": "../images/blue-shades-thumbnail.png",
-			"ItemImage": "../images/blue-shades.png",
-			"ItemPrice": 10,
-			"ItemID" : "item0",
-			"ModalTarget": "#item0modal",
-			"ModalId": "item0modal"
-		},
-		{
-			"ItemName": "Gold Shades",
-			"ItemThumbnail": "../images/gold-shades-thumbnail.png",
-			"ItemImage": "../images/gold-shades.png",
-			"ItemPrice": 10,
-			"ItemID" : "item1",
-			"ModalTarget": "#item1modal",
-			"ModalId": "item1modal"
-		}
-	]});
+  res.render('shop', data);
 };
