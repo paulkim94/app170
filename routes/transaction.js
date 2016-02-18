d38408adcf6e36e7f7e096e34227ea9fec1d2495
Paul Kim <pak013@ucsd.edu>
2016-02-17 19:15:58 -0800

    Able to add transactions in transaction.html

--- routes/transaction.js
+++ routes/transaction.js
@@ -1,7 +1,31 @@
 /*
  * GET transaction page
  */
+var data = require("../transaction.json");
 
 exports.view = function(req, res){
-  res.render('transaction', {});
+  res.render('transaction', data);
 };
+
+exports.addTransaction = function(req, res) {
+	// Your code goes here
+	var addedTransaction = {
+		"date": req.query.date,
+		"nameTransaction": req.query.nameTransaction,
+		"category": req.query.category,
+    "amount": req.query.amount
+	};
+
+	data["transactions"].push(addedTransaction);
+
+	res.render('transaction', data);
+  res.redirect('/transaction');
+
+}
+
+exports.deleteTransaction = function(req, res) {
+}
+
+exports.viewTransactions = function(req, res) {
+  res.json(data);
+}
