48eae1fb3abcc1dd213865974650edc4d1d212c2
Paul Kim <pak013@ucsd.edu>
2016-02-18 18:45:47 -0800

    Data from setup page added to home page

--- public/js/home.js
+++ public/js/home.js
@@ -6,12 +6,45 @@ var dailyAmnt = localStorage.getItem("dailyIndicatorAmnt");
 //$("#weeklyIndicatorNum").html("$" + weeklyAmnt);
 $("#weeklyIndicatorNum").html("$" + dailyAmnt);
 
-// Choose one of the descriptions
-var descriptionIndicator = "WEEK: 2/17 to 2/24 <br> SPENDING LIMIT: $" +
-                           weeklyAmnt + "<br><br> CURRENTLY REMAINING";
-descriptionIndicator = "TODAY: [DATE] <br> DAILY SPENDING LIMIT: $" +
-                       dailyAmnt + "<br> CURRENTLY SPENT: $0";
-
-$("#weeklyIndicatorDescription").html(descriptionIndicator);
 
 $("#divCircle").attr("class", "c100 p0 huge green");
+
+inputData();
+
+updateProgressBar();
+
+function inputData() {
+
+  /* Todays date */
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1;
+  var yyyy = today.getFullYear();
+
+  // Indicator description
+  var descriptionIndicator = "TODAY: " + mm + "/" + dd +"/" + yyyy +
+                              " <br> DAILY SPENDING LIMIT: $" +
+                         dailyAmnt + "<br> CURRENTLY SPENT: $0";
+
+  $("#weeklyIndicatorDescription").html(descriptionIndicator);
+
+  // Row 3: Goal description
+  var goalTitle = localStorage.getItem("goalTitle");
+  var daysLeft = localStorage.getItem("daysLeft");
+
+  var daysLeftDescription = daysLeft + " days until...";
+  //
+  $("#daysLeft").html(daysLeftDescription);
+  $("#goalName").html(goalTitle);
+
+}
+
+function updateProgressBar() {
+
+  var goalAmnt = localStorage.getItem("goalAmnt");
+
+  $("#goalProgress").attr("aria-valuenow", "0");
+  $("#goalProgress").attr("aria-valuemax", "'" + goalAmnt + "''");
+  $("#goalProgress").attr("style", "width: 0%");
+  $("#savedDescription").html("$0 / " + goalAmnt + " saved");
+}
